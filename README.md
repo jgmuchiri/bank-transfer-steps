99617d1badd6c4ced3498f552c4ca908a32bdf6c
jgmuchiri <jgmuchiri@gmail.com>
2019-02-01 21:54:52 -0500

    Update

---
+++ README.md
@@ -0,0 +1,2 @@
+![Screenshot(img/demo1.png "Screenshot")
+![Screenshot(img/demo2.png "Screenshot")
---
+++ overlay.php
@@ -0,0 +1,43 @@
+<div data-ct-busy-indicator="vm.isBusy" class="page-splash dissolve-animation ng-hide">
+    <div data-ct-spinner="vm.spinnerOptions">
+        <div class="spinner" role="progressbar"
+             style="position: absolute; width: 0px; z-index: 2000000000; left: 50%; top: 50%;">
+            <div
+                style="position: absolute; top: -15px; opacity: 0.25; animation: opacity-100-25-0-7 0.588235s linear infinite;">
+                <div
+                    style="position: absolute; width: 30px; height: 30px; box-shadow: rgba(0, 0, 0, 0.0980392) 0px 0px 1px; transform-origin: left 50% 0px; transform: rotate(0deg) translate(40px, 0px); border-radius: 15px; background: rgb(238, 238, 238);"></div>
+            </div>
+            <div
+                style="position: absolute; top: -15px; opacity: 0.25; animation: opacity-100-25-1-7 0.588235s linear infinite;">
+                <div
+                    style="position: absolute; width: 30px; height: 30px; box-shadow: rgba(0, 0, 0, 0.0980392) 0px 0px 1px; transform-origin: left 50% 0px; transform: rotate(51deg) translate(40px, 0px); border-radius: 15px; background: rgb(238, 238, 238);"></div>
+            </div>
+            <div
+                style="position: absolute; top: -15px; opacity: 0.25; animation: opacity-100-25-2-7 0.588235s linear infinite;">
+                <div
+                    style="position: absolute; width: 30px; height: 30px; box-shadow: rgba(0, 0, 0, 0.0980392) 0px 0px 1px; transform-origin: left 50% 0px; transform: rotate(102deg) translate(40px, 0px); border-radius: 15px; background: rgb(238, 238, 238);"></div>
+            </div>
+            <div
+                style="position: absolute; top: -15px; opacity: 0.25; animation: opacity-100-25-3-7 0.588235s linear infinite;">
+                <div
+                    style="position: absolute; width: 30px; height: 30px; box-shadow: rgba(0, 0, 0, 0.0980392) 0px 0px 1px; transform-origin: left 50% 0px; transform: rotate(154deg) translate(40px, 0px); border-radius: 15px; background: rgb(238, 238, 238);"></div>
+            </div>
+            <div
+                style="position: absolute; top: -15px; opacity: 0.25; animation: opacity-100-25-4-7 0.588235s linear infinite;">
+                <div
+                    style="position: absolute; width: 30px; height: 30px; box-shadow: rgba(0, 0, 0, 0.0980392) 0px 0px 1px; transform-origin: left 50% 0px; transform: rotate(205deg) translate(40px, 0px); border-radius: 15px; background: rgb(238, 238, 238);"></div>
+            </div>
+            <div
+                style="position: absolute; top: -15px; opacity: 0.25; animation: opacity-100-25-5-7 0.588235s linear infinite;">
+                <div
+                    style="position: absolute; width: 30px; height: 30px; box-shadow: rgba(0, 0, 0, 0.0980392) 0px 0px 1px; transform-origin: left 50% 0px; transform: rotate(257deg) translate(40px, 0px); border-radius: 15px; background: rgb(238, 238, 238);"></div>
+            </div>
+            <div
+                style="position: absolute; top: -15px; opacity: 0.25; animation: opacity-100-25-6-7 0.588235s linear infinite;">
+                <div
+                    style="position: absolute; width: 30px; height: 30px; box-shadow: rgba(0, 0, 0, 0.0980392) 0px 0px 1px; transform-origin: left 50% 0px; transform: rotate(308deg) translate(40px, 0px); border-radius: 15px; background: rgb(238, 238, 238);"></div>
+            </div>
+        </div>
+    </div>
+    <div class="page-splash-message page-splash-message-subtle">Please wait ...</div>
+</div>
